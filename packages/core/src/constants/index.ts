b77d5f838dc217192e1cb8274e957489a26e234b
v.sviridov <v_l_a_d_i_k1@mail.ru>
2025-09-01 23:21:00 +0300

    Rename Events, increase version

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -3,15 +3,16 @@ export const CHEKIN_IFRAME_TITLE = 'Chekin Host SDK';
 export const CHEKIN_IFRAME_NAME = 'chekin-host-sdk-frame';
 
 export const CHEKIN_EVENTS = {
-  HANDSHAKE: 'handshake',
-  HEIGHT_CHANGED: 'height-changed',
-  ERROR: 'error',
-  CONNECTION_ERROR: 'connection-error',
-  POLICE_ACCOUNT_CONNECTION: 'police-account-connection',
-  STAT_ACCOUNT_CONNECTION: 'stat-account-connection',
-  CONFIG_UPDATE: 'config-update',
-  ROUTE_CHANGED: 'route-changed',
-  INIT_ROUTE: 'init-route',
+  HANDSHAKE: 'chekin:handshake',
+  HEIGHT_CHANGED: 'chekin:height-changed',
+  ERROR: 'chekin:error',
+  CONNECTION_ERROR: 'chekin:connection-error',
+  POLICE_ACCOUNT_CONNECTION: 'chekin:police-account-connection',
+  STAT_ACCOUNT_CONNECTION: 'chekin:stat-account-connection',
+  CONFIG_UPDATE: 'chekin:config-update',
+  ROUTE_CHANGED: 'chekin:route-changed',
+  INIT_ROUTE: 'chekin:init-route',
+  READY: 'chekin:ready',
 } as const;
 
 export const LOG_LEVELS = {
